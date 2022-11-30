439a4216d7a74677d8f33b3f4ca682aee88500eb
adrianMartinezTeba <119281105+adrianMartinezTeba@users.noreply.github.com>
2022-11-30 23:44:07 +0100

    Add files via upload

---
+++ fundamentos-de-programacion-bikram-var-bucles-condicionales/index.js
@@ -0,0 +1,27 @@
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = PI*2;
+let booleanoAnd = booleano1 === booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2)&&(booleano1 ||(!booleano1 && !booleano2));
+let incrementarDesp = 2; 
+let resultadoDesp = incrementarDesp ++;
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes;
+let i = 0
+for (let contarHasta10 = 0;contarHasta10===10 ; contarHasta10_2++);
+/** ejercicio 10,11 y 12 no se hacerlos el de arriba es el 10 pero no se que tengo mal */
+let variableValorNumerico = 10;
+const MiNombre = "Adrian";
+const MiNumeroFav = 33;
+let booleanoOr = booleano1 || booleano2;
+let booleanoMix1 = (booleano1 && (TAU/2 == PI)) || (variableValorNumerico >= MiNumeroFav);
+let seisNoEsNueve = 6 !== 9;
+let booleanoMix2 = variableValorNumerico < (MiNumeroFav * TAU);
+let valorSuma = MiNumeroFav + variableValorNumerico;
+let valorResta = MiNumeroFav - variableValorNumerico;
+let valorMultiplicacion = MiNumeroFav*variableValorNumerico;
+let valorDivision =  MiNumeroFav/3;
+let contarHasta10 = 0
